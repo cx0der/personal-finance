03a0df7b06162e500e5d29dc6e0ad2b5434d2484
Shivakumar Thanikachalam <277487+cx0der@users.noreply.github.com>
2024-07-31 12:22:13 +0100

    [add-account] refactoring to follow new convention (#6)
    Refactor add-account sub command to follow the one command one action
    convention.
    Remove custom validation for AddAccountCommandArgs as it is no longer
    required. Subsequently removed the unit test case
    When cli arg parsing fails we try to print the usage for just the
    subcommand user was trying to execute

--- src/test/java/com/moveableapps/pf/commands/AddAccountCommandTest.java
+++ src/test/java/com/moveableapps/pf/commands/AddAccountCommandTest.java
@@ -7,6 +7,7 @@ import com.moveableapps.pf.entities.Account;
 import com.moveableapps.pf.entities.AccountType;
 import com.moveableapps.pf.utils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -18,39 +19,51 @@ class AddAccountCommandTest {
 
     private Repository repository;
     private BookKeeper bookKeeper;
+    private AddAccountCommand cmd;
 
     @BeforeEach
     void setUp() {
         repository = new MemoryRepository();
         bookKeeper = new BookKeeper(repository);
+        cmd = new AddAccountCommand();
     }
 
     @Test
-    void addAccount_returnsZero_goodCase() {
+    @DisplayName("Creates an account successfully")
+    void addAccountCreatesSuccessfully() {
         AddAccountCommandArgs args = new AddAccountCommandArgs("Income account", "Salary account", "INR", AccountType.INCOME);
-        AddAccountCommand cmd = new AddAccountCommand();
         int exitCode = cmd.execute(args, bookKeeper, System.out, System.err);
         assertEquals(0, exitCode);
     }
 
     @Test
-    void addAccount_existsWithOne_onDuplicateName() {
+    @DisplayName("Displays the name and id of the new account")
+    void addAccountPrintsNameAndId() {
+        AddAccountCommandArgs args = new AddAccountCommandArgs("Fuel", "Car fuel", "INR", AccountType.EXPENSE);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        cmd.execute(args, bookKeeper, new PrintStream(stream), System.err);
+        assertEquals("Created account: Fuel with id: 1\n", stream.toString());
+    }
+
+    @Test
+    @DisplayName("Fails for duplicate account with exit code 1")
+    void addAccountDuplicateTest() {
         Account account = TestUtils.getIncomeAccount();
         repository.addAccount(account);
         AddAccountCommandArgs args = new AddAccountCommandArgs(account.name(), account.description(), account.currency(), account.type());
-        AddAccountCommand cmd = new AddAccountCommand();
         int exitCode = cmd.execute(args, bookKeeper, System.out, System.err);
         assertEquals(1, exitCode);
     }
 
     @Test
-    void addAccount_printsError_onDuplicateName() {
+    @DisplayName("Displays error message for duplicate account")
+    void onDuplicatePrintsError() {
         Account account = TestUtils.getIncomeAccount();
         repository.addAccount(account);
         AddAccountCommandArgs args = new AddAccountCommandArgs(account.name(), account.description(), account.currency(), account.type());
 
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        AddAccountCommand cmd = new AddAccountCommand();
         cmd.execute(args, bookKeeper, System.out, new PrintStream(stream));
         assertEquals("Account Name is not unique\n", stream.toString());
     }
